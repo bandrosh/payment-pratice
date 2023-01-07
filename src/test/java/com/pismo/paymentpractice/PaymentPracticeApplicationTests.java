214c4a46444b05fb6645aa4a7f0dcc64186a2ea4
leonardo jales <bandrosh@gmail.com>
2023-01-07 16:27:56 -0300

    Added initial project

---
+++ src/test/java/com/pismo/paymentpractice/PaymentPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pismo.paymentpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
